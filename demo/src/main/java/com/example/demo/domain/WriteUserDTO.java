bf7111e99eb3271d114d8199912df08a8ac9c1d8
higakaga <higakaga@gmail.com>
2025-03-28 12:54:24 +0900

    likelion session2

---
+++ demo/src/main/java/com/example/demo/domain/WriteUserDTO.java
@@ -0,0 +1,11 @@
+package com.example.demo.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class WriteUserDTO {
+    private String username;
+    private String userpart;
+}
